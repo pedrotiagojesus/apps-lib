49f6ead482616798346fc7b7b67c748379fc5fae
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-07-30 00:41:10 +0100

    Update layout module "Weather"

--- src/components/Weather/Forecast.jsx
+++ src/components/Weather/Forecast.jsx
@@ -1,18 +1,32 @@
 import "./Forecast.css";
 
 const Forecast = ({ forecastList }) => {
+    // Prepare data
+    const dataArr = [];
+
+    forecastList.forEach((forecast) => {
+        console.log(forecast);
+
+        const date = new Date(forecast.dt * 1000);
+        const hours = date.getUTCHours();
+
+        dataArr.push({
+            temp: `${forecast.main.temp} ºC`,
+            image: `http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png`,
+            description: forecast.weather[0].description,
+            hour: hours,
+        });
+    });
+
     return (
         <div id="forescast">
             <h4>Forecast for the next few hours</h4>
             <ul>
-                {forecastList.map((forecast, i) => (
-                    <li key={i}>
-                        <img
-                            src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
-                            alt={forecast.weather[0].description}
-                        />
-                        {forecast.main.temp} ºC -{" "}
-                        {forecast.weather[0].description}
+                {dataArr.map((data, i) => (
+                    <li key={i} className="flex-fill" title={data.description}>
+                        <span>{data.temp}</span>
+                        <img src={data.image} alt={data.description} />
+                        {data.hour}h
                     </li>
                 ))}
             </ul>
