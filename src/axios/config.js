9cdc377a64844e713075008db572d9a33f7b9c9a
Pedro Jesus <pedrotiagojesus1995@gmail.com>
2024-07-30 18:36:03 +0100

    Bug fix module "ApiTranslate"

--- src/axios/config.js
+++ src/axios/config.js
@@ -4,12 +4,13 @@ const openWeatherMapFetch = axios.create({
     baseURL: "https://api.openweathermap.org/data/2.5",
 });
 
+const translateApiKey = import.meta.env.VITE_TRANSLATE_API_KEY || "";
+
 const translateFetch = axios.create({
     baseURL: "https://google-api31.p.rapidapi.com/gtranslate",
 });
 
-translateFetch.defaults.headers.common["x-rapidapi-key"] =
-    "52f60a1141msh61637c05d4a2c44p173419jsnaef9250f4179";
+translateFetch.defaults.headers.common["x-rapidapi-key"] = translateApiKey;
 translateFetch.defaults.headers.common["x-rapidapi-host"] =
     "google-api31.p.rapidapi.com";
 translateFetch.defaults.headers.common["Content-Type"] = "application/json";
